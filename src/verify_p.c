71cd4d275133da1ba85ceffb70d006dcd8670a35
Joao miguel Correia rito <joacorre@c1r20s13.42lisboa.com>
2022-06-23 14:04:16 +0100

    c

--- src/verify_p.c
+++ src/verify_p.c
@@ -23,8 +23,8 @@ void	verify_p(const char *format, int *i, va_list ptr, int *str_len)
 	nbr = va_arg(ptr, unsigned long long int);
 	if (nbr == 0)
 	{
-		ft_putstr_fd("(nil)", 1);
-		*str_len = *str_len + ft_strlen("(nil)");
+		ft_putstr_fd("0x0", 1);
+		*str_len = *str_len + ft_strlen("0x0");
 	}
 	else
 	{
